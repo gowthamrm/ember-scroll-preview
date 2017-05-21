748272b35188a77d322dd42a1b38bcf615885334
gowtham.mk <gowtham.mk@zohocorp.com>
2017-05-21 15:58:06 +0530

    Imports html2canvas from nodemodules directory

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -14,7 +14,7 @@ module.exports = function(defaults) {
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
 
-  app.import('bower_components/html2canvas/build/html2canvas.js')
+  app.import(app.nodeModulesDirectory + '/html2canvas/dist/html2canvas.min.js')
 
   return app.toTree();
 };
