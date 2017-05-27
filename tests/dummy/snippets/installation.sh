f452488fa6292adc67d26c01455c8bf6450ed502
gowtham.mk <gowtham.mk@zohocorp.com>
2017-05-28 01:07:48 +0530

    Updates the demo application

---
+++ tests/dummy/snippets/installation.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/gowthamrm/ember-scroll-progress.git
+cd ember-scroll-preview
+npm install
---
+++ tests/dummy/snippets/tests.sh
@@ -0,0 +1,3 @@
+npm test (Runs `ember try:each` to test your addon against multiple Ember versions)
+ember test
+ember test --server
