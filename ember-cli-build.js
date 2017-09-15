b4863b2652ee289da161f99267acffab4f9728ed
gowtham.mk <gowtham.mk@zohocorp.com>
2017-09-15 10:55:43 +0530

    Imports html2canvas directly from node_modules Used ember-inline-css

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,22 +6,13 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    nodeAssets: {
-      'html2canvas': {
-        vendor: {
-          srcDir: 'dist',
-          destDir: 'html2canvas',
-          include: ['html2canvas.min.js']
-        }
-      }
-    },
     snippetPaths: ['tests/dummy/snippets'],
     'ember-cli-babel': {
       includePolyfill: true
     }
   });
 
-  app.import('vendor/html2canvas/html2canvas.min.js');
+  app.import('node_modules/html2canvas/dist/html2canvas.min.js');
 
   return app.toTree();
 };
